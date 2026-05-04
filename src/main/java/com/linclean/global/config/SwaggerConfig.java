914805a52adbad86b4746ecf9c1b7d38c0090792
sunm2n <sunmin8410@naver.com>
2026-05-04 15:48:53 +0900

    [Feat] swagger 커스텀 세팅
    - API Info 설정
    - JWT Bearer 인증 추가
    - Authorization: Bearer <token> 헤더로 자동 첨부
    
    IssueNum #13

---
+++ src/main/java/com/linclean/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.linclean.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String BEARER_AUTH = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, bearerSecurityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("LinClean API")
+                .description("LinClean 서비스 API 문서")
+                .version("v1.0.0");
+    }
+
+    private SecurityScheme bearerSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+    }
+}
