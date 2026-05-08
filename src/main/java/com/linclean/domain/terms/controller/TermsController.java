be67a227bbc7f46e1336d1ceb28545d366ba995f
sunm2n <sunmin8410@naver.com>
2026-05-08 17:12:01 +0900

    [Feat] TermsControllerDocs 인터페이스로 Swagger 문서 분리
    IssueNum #17

--- src/main/java/com/linclean/domain/terms/controller/TermsController.java
+++ src/main/java/com/linclean/domain/terms/controller/TermsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/terms")
 @RequiredArgsConstructor
-public class TermsController {
+public class TermsController implements TermsControllerDocs {
 
     private final TermsService termsService;
 
