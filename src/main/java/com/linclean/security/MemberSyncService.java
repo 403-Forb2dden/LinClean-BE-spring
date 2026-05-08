922d68f3a8275d88d9044ae4512dd42537d29d09
sunm2n <sunmin8410@naver.com>
2026-05-08 17:33:07 +0900

    [Feat] MemberSyncService 동시 첫 인증 요청 race condition 처리
    IssueNum #16

--- src/main/java/com/linclean/security/MemberSyncService.java
+++ src/main/java/com/linclean/security/MemberSyncService.java
@@ -3,6 +3,7 @@ package com.linclean.security;
 import com.linclean.domain.member.entity.Member;
 import com.linclean.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +15,14 @@ public class MemberSyncService {
 
     @Transactional
     public Member findOrCreate(String clerkId) {
-        return memberRepository.findByClerkId(clerkId)
-                .orElseGet(() -> memberRepository.save(
-                        Member.builder().clerkId(clerkId).build()
-                ));
+        try {
+            return memberRepository.findByClerkId(clerkId)
+                    .orElseGet(() -> memberRepository.save(
+                            Member.builder().clerkId(clerkId).build()
+                    ));
+        } catch (DataIntegrityViolationException e) {
+            return memberRepository.findByClerkId(clerkId)
+                    .orElseThrow(() -> e);
+        }
     }
 }
